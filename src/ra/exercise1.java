f223b69b87b1dbd6df14764267f9a9e7df4fd1b8
phanquychau123 <quychau10a3@gmail.com>
2024-05-08 23:17:00 +0900

    addfile

---
+++ src/ra/exercise1.java
@@ -0,0 +1,12 @@
+package ra;
+
+import java.util.Scanner;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hay nhap ten cua ban");
+        String name = scanner.nextLine();
+        System.out.println("xin chao" + " " + name);
+    }
+}
